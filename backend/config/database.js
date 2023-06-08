13e81a31ad573aaac53d86eda729e9195017f9ea
Vinicius-Borges7 <vinicius.shr@gmail.com>
2023-06-07 21:45:09 -0300

    db and server config class done

---
+++ backend/config/database.js
@@ -0,0 +1,36 @@
+// Dependencies //
+require('dotenv').config();
+const dbUri = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
+
+// Main Code //
+class Database{
+    #dbUri;
+    #dbName;
+    #route;
+
+    constructor(route = "???"){
+        this.#dbUri = dbUri;
+        this.#dbName = dbName;
+        this.#route = route;
+    }
+
+    async connect(){
+        try {
+            await mongoose.connect(this.#dbUri, {
+                dbName: this.#dbName,
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            })
+
+            .then(() => {
+               console.log(this.#route + " Estabilished connection with Mongo") 
+            });
+        } catch(error){
+            console.error(error.message);
+            process.exit(1);
+        }
+    }
+}
+
+module.exports = Database;
---
+++ backend/config/server.js
@@ -0,0 +1,40 @@
+// Dependencies //
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+// Main Code //
+class Server {
+	#app;
+	#port;
+
+	constructor(app = undefined){
+		app = app;
+		port = PORT;
+	}
+
+	start(){
+		this.#app.listen(this.#port, () => {
+			try{
+				// adicionar coisas aqui para testar o server depois
+				console.log("app running on port " + this.#port);
+			} catch (error) {
+				console.log("FATAL ERROR IN APP:");
+				console.log(error);
+				console.log("STOPPING SERVER NOW");
+				this.#app.close();
+			}
+		});
+	}
+
+	stop(){
+		try{
+			this.#app.close();
+			console.log("Server stopped successfully");
+		} catch (error){
+			console.log("ERROR IN METHOD, CAN'T STOP THE SERVER:");
+			console.log(error);
+		}
+	}
+}
+
+module.exports = Server;
